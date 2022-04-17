26bbe2a898a4dcc15dd50f1a1b1a42e67b1d736a
Anikroy7 <roy645800@gmail.com>
2022-04-17 17:08:38 +0600

    added services data

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -8,15 +8,15 @@ import banner3 from '../../../../src/music-image/banner3.jpg'
 const Banner = () => {
     return (
         <Carousel className='mt-2'>
-            <Carousel.Item interval={1000}>
+            <Carousel.Item interval={500}>
                 <img
                     className="d-block w-100"
                     src={banner1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h1>My Own Project Enter Sandman</h1>
+                    <p>This is avialabe in youtube.this is the great song of mettalica. i compoosed it.I use diffent kind of sound for making this song</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={500}>
@@ -26,8 +26,8 @@ const Banner = () => {
                     alt="Second slide"
                 />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>My Upcoming Projects Time's</h3>
+                    <p>It will be avialabe in youtube and other social platfrom very soon.this is a sycodelic rock song composed by our own band.In this song i use a spicial instrument which sound is so cool.So,if you want to listen this song, please stay with us</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -37,8 +37,8 @@ const Banner = () => {
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    <h3>My Most Favourite Albumn</h3>
+                    <p>I released many kinds of album in this year. i works for many rock songs which is my best work. i coomposed those music with my own fiil. i use diffent types of instument for composed.Such as guitar, drums , bass, keyboard, ukelele etc.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
---
+++ src/Components/Home/Services/services.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const services = () => {
+    const services = [
+        { id: 1, name: "Voice Over", description: 'Voice-overs are used in video games and on-hold messages,[3] as well as for announcements and information at events and tourist destinations. It may also be read live for events such as award presentations.', price: 2000, ratings: "*****" },
+        { id: 1, name: "Mixing and Mastering", description: 'DynOne was designed from the ground up to put 5 bands of quality parallel compression at your fingertips. Transparent and adjustable filters will help control your audio levels without discarding the nuances of your sound. ', price: 5000, ratings: '*****' },
+        { id: 1, name: "Podcast Editing", description: 'Place the audio material in sequence (e.g. intro narration, interview, outro). When you import multiple tracks, the audio will appear on different tracks. Move it so that all the audio is on one track.' },
+        { id: 1, name: "Beat Making", description: 'Have you ever wondered what drum beat is the most famous in the world? I was definitely curious, so I did some research and found that the “Funky Drummer” beat by Clyde Stubblefield has been the most sampled drum groove ever (over 7 million times!).', price: 1500, ratings: '*****' },
+        { id: 1, name: "Vocal tuning", description: 'Over the last decade, autotune has evolved from a subtlety producers and engineers use to alter vocals and correct out of tune notes to a full-blown identity for certain musicians. Now, predominantly used in the realms of trap and left-field pop music, fans will even be able to identify an artist just by the way their distinct autotune sounds.', price: 3000, ratings: '****' },
+        { id: 1, name: "Audio ads Production", description: '. Although this highly sought after robotic effect can be created by a vocoder and talkbox it’s important not to get them confused. The vocoder, which was invented in 1938, synthesizes human speech using different band frequencies whereas a talkbox lets musicians shape the sound of an instrument using their mouth and a plastic tube.', price: 6000, ratings: '****' }
+
+    ]
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default services;
