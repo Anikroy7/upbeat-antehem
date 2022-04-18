f473c17977562bbfaa522785b6b6178077d791a6
Anikroy7 <roy645800@gmail.com>
2022-04-18 16:32:33 +0600

    create user with react firebase hook

--- src/Components/Login/Signup/Signup.js
+++ src/Components/Login/Signup/Signup.js
@@ -1,14 +1,14 @@
 import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Signup.css'
 const Signup = () => {
-
+    const [errorMessage, setErrorMessage] = useState('')
     const [
         createUserWithEmailAndPassword,
         user,
@@ -16,13 +16,28 @@ const Signup = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
 
+    const navigate = useNavigate();
+
+    if (user) {
+
+        navigate('/home')
+    }
+
+    if (error) {
+        setErrorMessage(error.message)
+    }
+
     const handelFromSubmit = event => {
         event.preventDefault()
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(password);
+        if (password.length < 6) {
+            setErrorMessage('Give at least 6 digits')
+        }
+        else {
+            createUserWithEmailAndPassword(email, password)
+        }
 
-        createUserWithEmailAndPassword(email, password)
     }
 
 
@@ -38,7 +53,7 @@ const Signup = () => {
                             <FontAwesomeIcon className='mt-2 me-2' icon={faUser}></FontAwesomeIcon>
                         </span>
                         <input className='border-0 border-bottom w-75 text-black p-2' placeholder='
-                        Type your username' type="text" name="name" id="name" />
+                        Type your username' type="text" name="name" id="name" required />
                     </p>
                     <p className='ms-5 mt-2'>
                         <span className='text-black d-block mb-0'>Email</span>
@@ -46,7 +61,7 @@ const Signup = () => {
                             <FontAwesomeIcon className='mt-2 me-2' icon={faUser}></FontAwesomeIcon>
                         </span>
                         <input className='border-0 border-bottom w-75 text-black p-2' placeholder='
-                        Type your Email' type="email" name="email" id="email" />
+                        Type your Email' type="email" name="email" id="email" required />
                     </p>
                     <p className='ms-5 mt-2 '>
                         <span className='text-black d-block mb-0'>Password</span>
@@ -54,12 +69,15 @@ const Signup = () => {
                             <FontAwesomeIcon className='mt-2 me-2' icon={faLock}></FontAwesomeIcon>
                         </span>
                         <input className='border-0 border-bottom w-75 text-black p-2' placeholder='
-                        Type your password' type="password" name="password" id="password" />
+                        Type your password' type="password" name="password" id="password" required />
                         <p className='text-black mt-3 '>Forgot password?</p>
                         <p className='text-black'>
                             <span>Already have an account? </span>
                             <span><Link to={'/login'} className='text-black d-inline fw-bold'> Login</Link></span>
                         </p>
+                        {
+                            <p className='text-danger'>!{errorMessage}</p>
+                        }
                     </p>
                     <p className=' d-flex justify-content-center mt-3'>
                         <input className='from-button w-50 ' type="submit" value="Sign up" />
